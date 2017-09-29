cb09e54edc504c82ad674caca526a62ff169d371
Austin Brown <austin.nils@gmail.com>
2017-09-28 18:07:48 -0700

    Initial commit

---
+++ rappy/Rappy.py
@@ -0,0 +1,73 @@
+import re
+import colored
+
+class Rappy:
+    def __init__(self, dictionary_file):
+        '''
+        Creating a new object from a dictionary file will scan that dictionary
+        and convert it to a python dict for easier use when searching for words.
+        '''
+        self.dictionary_file = dictionary_file
+        self.dictionary_file = open(self.dictionary_file, 'r')
+
+        self.dictionary = {}
+
+        for line in self.dictionary_file:
+            words = line.split()
+            val = []
+
+            for i in range(len(words)):
+                if(i is not 0):
+                    val.append(words[i])
+
+            self.dictionary[words[0]] = val
+
+    def get_syllables(self, word):
+        '''
+        Attempts to look the word up in the dictionary and return a tuple of
+        the input word and the accompanying syllables from the dictionary. 
+        Uses Regex.
+        '''
+        original = word
+        
+        try:
+            syllables = self.dictionary[self.prepare_word(word)]
+            return original, syllables
+        except:
+            return original, False
+
+    def prepare_word(self, word):
+        '''
+        Remove invalid characters from the word and convert it to uppercase to
+        match the dictionary. 
+        '''
+        return re.sub(',.;!', '', word).upper()
+
+    def colorize(self, lyric_file):
+        '''
+        Find matching words in the lyric_file and assign them the same color value. Uses the
+        "colored" package.
+        '''
+        with open(lyric_file, 'r') as f:
+            color_key = {}
+            color_id = 1
+            data = re.findall(r'\S+|\n', f.read())
+
+            for i in range(len(data)):
+                prepared_word = self.prepare_word(data[i])
+                orig_word, syllables = self.get_syllables(prepared_word)
+                
+                if prepared_word is '\n':
+                    print(prepared_word)
+                else:
+                    end_fg = colored.attr(0)
+
+                    if prepared_word in color_key:
+                        bg = colored.bg(color_key[prepared_word])
+                    else:
+                        color_key[prepared_word] = color_id
+                        color_id = color_id + 1
+                        bg = colored.bg(color_key[prepared_word])
+                    
+                    out = "{}{}{}{}".format(colored.fg('black'),bg, orig_word, end_fg)
+                    print(out, end=' ')
---
+++ rappy/main.py
@@ -0,0 +1,5 @@
+from Rappy import Rappy
+
+Rappy = Rappy('rappy/cmudict-0.7b')
+Rappy.get_syllables('ride')
+Rappy.colorize('rappy/example/rap.txt')
