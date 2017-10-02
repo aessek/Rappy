bd4659b7b11653a32bf55e18cd43f0573ee31d07
Austin Brown <austin.nils@gmail.com>
2017-10-02 15:42:22 -0700

    Change the color scheme

--- rappy/main.py
+++ rappy/main.py
@@ -5,6 +5,6 @@ with open('rappy/example/output.html', 'w') as f:
     Rappy = Rappy('rappy/cmudict-0.7b')
     Rappy.get_syllables('ride')
 
-    colors = Rappy.colorize('rappy/example/rap.txt', min_h = 50, max_h = 50, min_s = 90, min_v = 50, max_v = 60)
+    colors = Rappy.colorize('rappy/example/rap.txt', min_h = 0, max_h = 20, min_s = 98, min_v = 80, max_v = 82)
     # pprint(colors)
     f.write(Rappy.out_html('rappy/example/rap.txt', colors))
